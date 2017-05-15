95607ea93b1b20e59dbe5cb84e75941c20bf71d3
bauGid <fun-man2000@web.de>
2017-05-15 19:13:43 +0200

    Quicksort vorbereitet

---
+++ src/visSort/QuickSort.java
@@ -0,0 +1,27 @@
+package visSort;
+
+/**
+ * Created by Gideon on 15.05.2017.
+ */
+//TODO Quicksort implementieren und einbinden
+public class QuickSort implements Sorter {
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void addArray(int[] toBeSorted) {
+
+    }
+
+    @Override
+    public int[] getCurrentStatus() {
+        return new int[0];
+    }
+
+    @Override
+    public void sortStep() {
+
+    }
+}
